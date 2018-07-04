a1262f6660be734726a5e25170879f22a74f8d55
Jarrod Seccombe <Jarrod.Seccombe@gmail.com>
2018-07-04 01:43:26 -0400

    simplify scraper

--- fake_person_creator/scraper.py
+++ fake_person_creator/scraper.py
@@ -4,9 +4,7 @@ from bs4 import BeautifulSoup
 
 def get_page_data():
     page = requests.get('https://fakepersongenerator.com')
-    if page.status_code == 200:
-        return get_page_object(page)
+    if page.status_code != 200:
+        return None
 
-
-def get_page_object(page):
     return BeautifulSoup(page.content, 'html.parser')
